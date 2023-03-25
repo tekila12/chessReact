12bbffe4bb92d9288f3710bd1e39d4ac087b6035
Ajnur <radovic.ajnur96@gmail.com>
2023-03-25 14:54:17 +0100

    first commit

--- chess/client/src/App.tsx
+++ chess/client/src/App.tsx
@@ -2,7 +2,8 @@ import React, { Suspense } from 'react';
 import styles from "@/styles/Home.module.css";
 import { Canvas, } from '@react-three/fiber'
 import { Center, OrbitControls, useProgress } from '@react-three/drei';
-import ChessBoard from './components/Chessboard';
+import ChessBoard from './components/ChessBoard';
+
 
 
 
